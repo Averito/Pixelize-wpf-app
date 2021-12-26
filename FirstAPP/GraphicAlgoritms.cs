0cfedb67496c4e072507460e0679de79b8aa44a0
Avenant <avenantmar@gmail.com>
2021-12-27 01:03:50 +0300

    pixelize wpf app

---
+++ FirstAPP/GraphicAlgoritms.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+using System.IO;
+
+
+namespace FirstAPP
+{
+	public class GraphicAlgoritms
+	{
+		public static Bitmap Pixelize(Bitmap image, int pixelDegree)
+		{
+			if (pixelDegree < 1) throw new ArgumentOutOfRangeException("Слишком маленькое число, требуется не менее 1.");
+			int pixelizeStep = pixelDegree;
+
+			for (int x = 0; x < image.Width; x += pixelizeStep)
+			for (int y = 0; y < image.Height; y += pixelizeStep)
+			{
+				var currentColor = image.GetPixel(x, y);
+
+				int remainderPixelizeWidth = pixelizeStep;
+				int remainderPixelizeHeight = pixelizeStep;
+				if (x + pixelizeStep > image.Width)
+				{
+					remainderPixelizeWidth = image.Height - x;
+				}
+
+				if (y + pixelizeStep > image.Height)
+				{
+					remainderPixelizeHeight = image.Height - y;
+				}
+
+				for (int xTwo = 0; xTwo < remainderPixelizeWidth; xTwo++)
+				for (int yTwo = 0; yTwo < remainderPixelizeHeight; yTwo++)
+				{
+					image.SetPixel(x + xTwo, y + yTwo, currentColor);
+				}
+			}
+
+			return image;
+		}
+
+		public static BitmapImage BitmapToImageSource(Bitmap bitmap)
+		{
+			using (MemoryStream memory = new MemoryStream())
+			{
+				bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+				memory.Position = 0;
+				BitmapImage bitmapimage = new BitmapImage();
+				bitmapimage.BeginInit();
+				bitmapimage.StreamSource = memory;
+				bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+				bitmapimage.EndInit();
+
+				return bitmapimage;
+			}
+		}
+	}
+}
---
+++ FirstAPP/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+
+namespace FirstAPP
+{
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button1_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				int pixelizeDegreeInt;
+				if (pixelizeDegree.Text == string.Empty || int.TryParse(pixelizeDegree.Text, out pixelizeDegreeInt) == false)
+				{
+					MessageBox.Show("Ожидается целое число (степень пикселизации)");
+					return;
+				}
+			
+				Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+			
+				dlg.DefaultExt = ".png";
+				dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+				Nullable<bool> result = dlg.ShowDialog();
+
+				if (result == false) return;
+
+				Bitmap beforeImg = new Bitmap(dlg.FileName);
+				Bitmap afterImg = GraphicAlgoritms.Pixelize(beforeImg, pixelizeDegreeInt);
+				BitmapImage normalizeImg = GraphicAlgoritms.BitmapToImageSource(afterImg);
+				
+				string filename = dlg.FileName;
+				image1.Source = normalizeImg;
+			}
+			catch (ArgumentOutOfRangeException err)
+			{
+				MessageBox.Show(err.Message);
+			}
+		}
+	}
+}

e19bcb1bb38cbec4b1a57d5276a71c6cc5614e24
Avenant <avenantmar@gmail.com>
2021-12-27 01:56:48 +0300

    fix pixelize algoritm

--- FirstAPP/GraphicAlgoritms.cs
+++ FirstAPP/GraphicAlgoritms.cs
@@ -3,6 +3,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Drawing;
 using System.IO;
+using System.Windows;
 
 
 namespace FirstAPP
@@ -12,13 +13,13 @@ namespace FirstAPP
 		public static Bitmap Pixelize(Bitmap image, int pixelDegree)
 		{
 			if (pixelDegree < 1) throw new ArgumentOutOfRangeException("Слишком маленькое число, требуется не менее 1.");
+			if (pixelDegree == 1) return image;
+			
 			int pixelizeStep = pixelDegree;
 
 			for (int x = 0; x < image.Width; x += pixelizeStep)
 			for (int y = 0; y < image.Height; y += pixelizeStep)
 			{
-				var currentColor = image.GetPixel(x, y);
-
 				int remainderPixelizeWidth = pixelizeStep;
 				int remainderPixelizeHeight = pixelizeStep;
 				if (x + pixelizeStep > image.Width)
@@ -31,10 +32,14 @@ namespace FirstAPP
 					remainderPixelizeHeight = image.Height - y;
 				}
 
+				if (x >= image.Width) MessageBox.Show("Попався");
+
+				var currentColor = image.GetPixel(Clamp(x, image.Width), Clamp(y, image.Height));
+
 				for (int xTwo = 0; xTwo < remainderPixelizeWidth; xTwo++)
 				for (int yTwo = 0; yTwo < remainderPixelizeHeight; yTwo++)
 				{
-					image.SetPixel(x + xTwo, y + yTwo, currentColor);
+					image.SetPixel(Clamp(x + xTwo, image.Width), Clamp(y + yTwo, image.Height), currentColor);
 				}
 			}
 
@@ -55,6 +60,13 @@ namespace FirstAPP
 
 				return bitmapimage;
 			}
+		}
+
+		private static int Clamp(int first, int second)
+		{
+			if (first.CompareTo(second) >= 0) return second - 1;
+			if (first.CompareTo(second) < 0) return first;
+			return 0;
 		}
 	}
 }
